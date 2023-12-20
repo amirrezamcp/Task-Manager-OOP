d89adb303ba2476e131b0e78d210d5215123bce0
bigpotato-yt <138766025+bigpotato-yt@users.noreply.github.com>
2023-12-20 16:43:23 +0330

    Filing-and-static-UI

---
+++ bootstrap/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases_config = (object) [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'db' => 'todolist'
+];
+
+ // Name Host
+ // User Name
+ // Password
+ // Database Name
---
+++ prosses/ajaxHandler.php
@@ -0,0 +1,2 @@
+<?php
+include_once "../bootstrap/init.php";
